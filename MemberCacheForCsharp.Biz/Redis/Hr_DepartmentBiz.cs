29f1a7c8a954f5b570e6f8e3d0c3a1c9e3883d0d
wujian <wujian@innjia.com>
2017-12-19 18:14:52 +0800

    add redis 引用

---
+++ MemberCacheForCsharp.Biz/Redis/Hr_DepartmentBiz.cs
@@ -0,0 +1,21 @@
+﻿using MemberCacheForCsharp.Service.Imp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MemberCacheForCsharp.Model;
+
+namespace MemberCacheForCsharp.Redis.Biz
+{
+    public class Hr_DepartmentBiz
+    {
+        Hr_DepartmentService Hr_DepartmentService { get; set; }
+
+        public List<Hr_department> GetList()
+        {
+            List<Hr_department> list = new List<Hr_department>();
+            return list;
+        }
+    }
+}

fae86c247d88f88a5e83a6d0aeb0d2c33640e75e
wujian <wujian@innjia.com>
2017-12-19 10:14:27 +0800

    update

--- MemberCacheForCsharp.Service/IHr_DepartmentService.cs
+++ MemberCacheForCsharp.Service/IHr_DepartmentService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MemberCacheForCsharp.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace MemberCacheForCsharp.Service
 {
     public interface IHr_DepartmentService
     {
+        bool Add(Hr_department model);
+        bool Delete(int id);
+        bool Update(Hr_department model);
+        List<Hr_department> GetList();
+        Hr_department GetModel(string depNo);
     }
 }
--- MemberCacheForCsharp.Service/Imp/Hr_DepartmentService.cs
+++ MemberCacheForCsharp.Service/Imp/Hr_DepartmentService.cs
@@ -1,12 +1,68 @@
-﻿using System;
+﻿using MemberCacheForCsharp.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySqlSugar;
 
 namespace MemberCacheForCsharp.Service.Imp
 {
     public class Hr_DepartmentService : IHr_DepartmentService
     {
+        public SugarDao Dao { get; set; }
+        public bool Add(Hr_department model)
+        {
+            bool result = false;
+            using (var db = Dao.GetDao())
+            {
+                var rows = db.Insert(model);
+                if (rows != null)
+                {
+                    int count = Convert.ToInt32(rows);
+                    if (count > 0)
+                    {
+                        result = true;
+                    }
+                }
+            }
+            return result;
+        }
+        public bool Delete(int id)
+        {
+            bool result = false;
+            using (var db = Dao.GetDao())
+            {
+                result = db.Update<Hr_department>(new { IsDelete = 1, ModifyTime = DateTime.Now }, it => it.Id == id);
+            }
+            return result;
+        }
+        public bool Update(Hr_department model)
+        {
+            bool result = false;
+            using (var db = Dao.GetDao())
+            {
+                result = db.Update(model);
+            }
+            return result;
+        }
+        public List<Hr_department> GetList()
+        {
+            List<Hr_department> list = null;
+            using (var db = Dao.GetDao())
+            {
+                list = db.Queryable<Hr_department>().Where(o => o.IsDelete == 0).ToList();
+            }
+            return list;
+        }
+        public Hr_department GetModel(string depNo)
+        {
+            Hr_department model = null;
+            using (var db = Dao.GetDao())
+            {
+                model = db.Queryable<Hr_department>().Where(o => o.DepNo == depNo).FirstOrDefault();
+            }
+            return model;
+        }
     }
 }
